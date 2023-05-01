68ec5a9fec9473be0e9f10ff6e68db60977d0d1f
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-05-01 18:08:35 +0300

    add js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 const calculator = document.querySelector('.calculator')
+const output = calculator.querySelector('.calc_output')
+const clearBtn = calculator.querySelector('button[data-action="clear"]');
+
+const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ','];
+const operation = ['±', '%', '÷', '×', '-', '+', '=']
+
+let a = ''
+let b = ''
+let sign = ''
+
+calculator.addEventListener('click', onClearBtn)
+
+function onClearBtn(event) {
+    if (event.target === clearBtn) {
+        a = ''
+        b = ''
+        output.textContent = 0
+    }
+}
+
 
 calculator.addEventListener('click', onCalcBtn)
 
@@ -6,6 +26,38 @@ function onCalcBtn(event) {
     if (event.target.nodeName != "BUTTON") {
         return
     }
+    if (event.target === clearBtn) {
+        return
+    }
+    
 
+    if (digits.includes(event.target.textContent)) {
+        if (sign === '') {
+            a += event.target.textContent
+            output.textContent = a
+            console.log(`a: ${a}`);
+        }
+        if (sign != '') {
+            b += event.target.textContent
+            output.textContent = b
+            console.log(`b: ${b}`);
+        }
+    }
+
+    if (a.split('').includes(',') || b.split('').includes(',')) {
+    return
+    }
+
+
+    if (operation.includes(event.target.textContent)) {
+        sign = event.target.textContent
+console.log(sign);
+    }
+
+
+
+
+
+    
     console.log(event.target.textContent);
 } 
