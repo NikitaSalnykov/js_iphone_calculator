3111f554d827cc586c625b06b5be03929e538a82
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-05-02 01:51:49 +0300

    js update

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const calculator = document.querySelector('.calculator')
 const output = calculator.querySelector('.calc_output')
 const clearBtn = calculator.querySelector('button[data-action="clear"]');
+const resultBtn = calculator.querySelector('button[data-action="result"]')
 
-const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ','];
+const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 const operation = ['±', '%', '÷', '×', '-', '+', '=']
 
 let a = ''
 let b = ''
 let sign = ''
+let result = null
 
 calculator.addEventListener('click', onClearBtn)
 
@@ -19,6 +21,37 @@ function onClearBtn(event) {
     }
 }
 
+calculator.addEventListener('click', onResultButton)
+
+function onResultButton (event) {
+    if (event.target !== resultBtn) {
+        return
+    }
+
+    if (a !== '' && b !== '' && sign !== '' && operation.includes(event.target.textContent)) {
+        switch (sign) {
+            case '×':
+                result = Number(a) * Number(b)
+                break;
+            case '+':
+                result = Number(a) + Number(b)
+                break;
+            case '÷':
+                result = Number(a) / Number(b)
+                break;
+            case '-':
+                result = Number(a) - Number(b)
+                break;
+        }
+
+        output.textContent = result
+
+        a = result
+        b = ''
+        sign = ''
+    }
+
+}
 
 calculator.addEventListener('click', onCalcBtn)
 
@@ -26,38 +59,40 @@ function onCalcBtn(event) {
     if (event.target.nodeName != "BUTTON") {
         return
     }
-    if (event.target === clearBtn) {
+    if (event.target === clearBtn || event.target === resultBtn) {
         return
     }
     
+    if (output.textContent.includes('.') && event.target.textContent === '.') {
+    return;
+    } 
+
+    if (a === '' && event.target.textContent === '.') {
+    a = '0'
+    }
+    
+    if (a === '' && event.target.textContent === '0' || b === '0' && event.target.textContent === '0') {
+        return
+    }
 
     if (digits.includes(event.target.textContent)) {
-        if (sign === '') {
+        if (sign === '' && a !== result) {
             a += event.target.textContent
             output.textContent = a
             console.log(`a: ${a}`);
-        }
-        if (sign != '') {
+        } else {
             b += event.target.textContent
             output.textContent = b
             console.log(`b: ${b}`);
         }
     }
 
-    if (a.split('').includes(',') || b.split('').includes(',')) {
-    return
-    }
-
-
     if (operation.includes(event.target.textContent)) {
         sign = event.target.textContent
-console.log(sign);
+        if (sign != '') {
+            
+        }
     }
-
-
-
-
-
     
     console.log(event.target.textContent);
 } 
